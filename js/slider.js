f776d79ceb8ed0e4d28c477435c1274b0da9cc91
corletey <corleteya@gmail.com>
2024-06-12 15:49:42 +0000

    Update products

--- js/slider.js
+++ js/slider.js
@@ -1,35 +1,16 @@
-// // to get current year
-// function getYear() {
-//     var currentDate = new Date();
-//     var currentYear = currentDate.getFullYear();
-//     document.querySelector("#displayYear").innerHTML = currentYear;
-// }
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
 
-// getYear();
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
 
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
 
-
-// // slick slider
-// $('.chocolate_container').slick({
-//     infinite: true,
-//     center: true,
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     responsive: [{
-//             breakpoint: 991,
-//             settings: {
-//                 slidesToShow: 2,
-//                 slidesToScroll: 1
-//             }
-//         },
-//         {
-//             breakpoint: 576,
-//             settings: {
-//                 slidesToShow: 1,
-//                 slidesToScroll: 1
-//             }
-//         }
-
-//     ]
-// });
-
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
